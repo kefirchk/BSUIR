aaaf39f7597df3cb1e1780d17adb40f2acfea14b
Alexei Klimovich <90150234+kefirchk@users.noreply.github.com>
2024-01-13 11:31:32 +0300

    Add files via upload

---
+++ 1 semester/Basics of Algorithmization and Programming/Labs/laba3/functions.h
@@ -0,0 +1,4 @@
+int func1(int n, int i, int max, int j, int ans);                      //функция ввода размера массива
+void func2(int i, int n, int* array, int max, int j, int ans);         //функция ввода элементов массива
+void algorithm(int max, int n, int array[n], int i, int j, int ans);   //функция алгоритма решения основной части задачи и вывода на экран результата
+int *memory(int n, int i, int max, int j, int ans);                    //функция выделения памяти под массив
